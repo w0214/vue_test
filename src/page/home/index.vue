c24fce21729363a7a7d8437a6142009c3c9ea3e5
zzq <2502003387@qq.com>
2021-09-24 11:03:19 +0800

    测试elementui是否添加成功

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,13 +1,67 @@
 <template>
     <div class="home">
         <h1>测试功能</h1>
+        <div class="radio">
+            排序：
+            <el-radio-group v-model="reverse">
+            <el-radio :label="true">倒序</el-radio>
+            <el-radio :label="false">正序</el-radio>
+            </el-radio-group>
+        </div>
+        <div class="line">
+            <el-timeline :reverse="reverse">
+                <el-timeline-item
+                    v-for="(activity, index) in activities"
+                    :key="index"
+                    :timestamp="activity.timestamp">
+                    {{activity.content}}
+                </el-timeline-item>
+            </el-timeline>
+        </div>
+        
     </div>
 </template>
 <script>
 export default {
-    
+    name:'home',
+    data() {
+        return{
+            reverse: true,
+            activities: [
+                {
+                    content: '活动按期开始',
+                    timestamp: '2018-04-15'
+                }, 
+                {
+                    content: '通过审核',
+                    timestamp: '2018-04-13'
+                }, 
+                {
+                    content: '创建成功',
+                    timestamp: '2018-04-11'
+                }
+            ]
+        }
+    },
+    created() {
+
+    },
+    mounted(){
+
+    },
+    watch: {
+
+    },
+    methods() {
+
+    },
+
+
 }
 </script>
 <style  scoped>
-
+    .line {
+        width: 50%;
+        margin: 0 auto;
+    }
 </style>
